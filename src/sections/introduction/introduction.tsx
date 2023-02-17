f150bb0780e647e552cb02cf8772e67b6a184f50
ZeroX-DG <viethungax@gmail.com>
2023-02-18 07:20:44 +1300

    update

--- src/sections/introduction/introduction.tsx
+++ src/sections/introduction/introduction.tsx
@@ -4,6 +4,6 @@ import './introduction.scss';
 export const Introduction = () => (
     <div id="introduction">
         <p id="name">Viet-Hung Nguyen</p>
-        <p id="title">Software Engineer / Cinema & Music Enthusiast / Amateur Writer</p>
+        <p id="title">Software Engineer / Film Enthusiast</p>
     </div>
 );
