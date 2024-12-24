7c9aa4ce46c50ac581101cdc10e19aa15b1e3bfd
ZeroX-DG <viethungax@gmail.com>
2024-12-24 13:01:38 +1300

    removed unnecessary titles

--- src/sections/introduction/introduction.tsx
+++ src/sections/introduction/introduction.tsx
@@ -4,6 +4,5 @@ import './introduction.scss';
 export const Introduction = () => (
     <div id="introduction">
         <p id="name">Viet-Hung Nguyen</p>
-        <p id="title">Software Engineer / Film Enthusiast</p>
     </div>
 );
